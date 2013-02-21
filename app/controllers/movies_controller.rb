c3f4487c6880d67efc728f3e8873818601a0821d
Max Brody <maxabrody@gmail.com>
2013-02-20 23:42:41 -0500

    yeah

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,43 @@
 
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+
+    @all_ratings = Movie.ratings
+
+    if session[:ratings]
+      @ratings = session[:ratings]
+      @movies = Movie.find_all_by_rating(@ratings)
+    else
+      @movies = Movie.all
+    end
+
+    if params[:ratings]
+      session[:ratings] = params[:ratings].keys
+      @ratings = session[:ratings]
+      @movies = Movie.find_all_by_rating(@ratings)
+    end
+
+    if params[:sort_by] == 'movies'
+      session[:sort_by] = 'movies'
+    end
+
+    if params[:sort_by] == 'release_date'
+      session[:sort_by] = 'release_date'
+    end
+    
+    if session[:sort_by] == 'movies'
+      @movies.sort! { |a,b| a.title.downcase <=> b.title.downcase }
+      @sort_by_movie = true
+      @sort_by_release_date = false
+    end
+
+    if session[:sort_by] == 'release_date'
+      @movies = Movie.find(:all, order: 'release_date')
+      @movies = Movie.find_all_by_rating(@ratings)
+      @sort_by_release_date = true
+      @sort_by_movie = false
+    end
+
   end
 
   def show
